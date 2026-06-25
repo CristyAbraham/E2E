60c203e1cdac6c856becbc494b2791dc373a4916
Cristy <chriselsa94@gmail.com>
2026-06-25 09:32:53 +0530

    Fixed search functionality and updated test cases

--- POM/search.js
+++ POM/search.js
@@ -11,6 +11,14 @@ export class search
     this.product= page.locator("//p[contains(text(),'Stylish Dress')]/ancestor::div[@class='product-image-wrapper']//a[contains(text(),'View Product')]")
     this.productclick= page.locator(".btn.btn-default.cart")
     this.viewcart= page.locator("//u[text()='View Cart']")
+    //add multiple product
+   this.continue= page.getByRole('button',{name:'Continue Shopping'})
+    this.searchprod2= page.locator("//a[@href='/products']")
+    this.dress2view= page.locator("(//p[normalize-space()='Summer White Top'])[1]")
+    this.dress2click= page.locator("//p[text()='Summer White Top']/ancestor::div[@class='productinfo text-center']//a[contains(text(),'Add to cart')]")
+    //const product = await page.locator('.product-image-wrapper', {hasText: 'Summer White Top'});
+    //await product.locator('text=Add to cart').click();
+    this.exit= page.locator("//a[text()=' Logout']")
     }
     async viewCart(data){
         await this.searchbar.click();
@@ -19,7 +27,22 @@ export class search
         await this.product.click()
         await this.productclick.click()
         await this.viewcart.click()
+        await this.exit.click()
+    }
+    async multipleProduct(data){
 
-        
+        await this.searchbar.click();
+        await this.searchprod.fill(data.search)
+        await this.searchbtn.click()
+        await this.product.click()
+        await this.productclick.click()
+        //await this.viewcart.click()
+        await this.continue.click()
+        await this.searchprod2.click()
+        await this.dress2view.scrollIntoViewIfNeeded();
+        await this.dress2click.click()
+        await this.viewcart.click()
+        await this.exit.click()
+
     }
 }
--- tests/Add product/Search.spec.js
+++ tests/Add product/Search.spec.js
@@ -3,34 +3,21 @@ import {search} from '../../POM/search.js'
 import searchData from '../../Testdata/searchdata.json'
 import {Loginpage} from '../../POM/Loginpage.js'
 import logindata from '../../Testdata/logindata.json'
-test('search product and add cart',async({page})=>{
-    const searchpage=new search(page)
-    const loginPage=new Loginpage(page)
+test.beforeEach(async({page})=>{
+const loginPage=new Loginpage(page)
    await page.goto(searchData.url,{ waitUntil: 'domcontentloaded'})
     
    await loginPage.login(logindata.username,logindata.password)
-  await searchpage.viewCart(searchData)
+})
+test('search product and add cart',async({page})=>{
+    const searchpage=new search(page)
     
+  await searchpage.viewCart(searchData)
     
 })
 test('multiple products',async({page})=>{
-await page.goto("https://automationexercise.com/signup")
-    await page.locator("//input[@data-qa='login-email']").fill('ira@mailinator.com')
-    await page.locator("//input[@placeholder='Password']").fill("Ira@123")
-    await page.getByRole('button',{name:'Login'}).click()
-    await page.locator("//a[@href='/products']").click()
-    await page.getByPlaceholder('Search Product').fill("dress")
-    await page.locator("//button[@id='submit_search']").click()
-
-   await page.locator("//p[contains(text(),'Stylish Dress')]/ancestor::div[@class='product-image-wrapper']//a[contains(text(),'View Product')]").click()
-    await page.locator(".btn.btn-default.cart").click()
-    await page.getByRole('button',{name:'Continue Shopping'}).click()
-    await page.locator("//a[@href='/products']").click()
-    await page.locator("(//p[normalize-space()='Summer White Top'])[1]").scrollIntoViewIfNeeded();
-    await page.locator("//p[text()='Summer White Top']/ancestor::div[@class='productinfo text-center']//a[contains(text(),'Add to cart')]").click()
-    //const product = await page.locator('.product-image-wrapper', {hasText: 'Summer White Top'});
-    //await product.locator('text=Add to cart').click();
-    await page.locator("//a[text()=' Logout']").click()
-    
+const searchpage=new search(page)
+await searchpage.multipleProduct(searchData)
+   
 
 })
